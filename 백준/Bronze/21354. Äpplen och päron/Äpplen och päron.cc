ff5b4a9fb9b0eaca51b31f6fe0a958d41dff486b
HyunV <98259740+HyunV@users.noreply.github.com>
2024-12-26 17:38:30 +0900

    [Bronze IV] Title: Äpplen och päron, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21354. Äpplen och päron/Äpplen och päron.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, p;
+	cin >> a >> p;
+
+	a *= 7;
+	p *= 13;
+
+	if(a > p)
+		cout << "Axel";
+	else if(a < p)
+		cout << "Petra";
+	else if( a == p)
+		cout << "lika";
+}
