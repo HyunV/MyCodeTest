17352edd80ce4aafd931250003106278bb3092fc
HyunV <98259740+HyunV@users.noreply.github.com>
2025-01-30 13:51:11 +0900

    [Silver IV] Title: 대칭 차집합, Time: 288 ms, Memory: 21312 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,33 +1,36 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int main()
 {
 	int n, m;
 
-	unordered_map<int, bool> uMap;
+	unordered_set<int> uSet;
 
 	cin >> n >> m;
 	int num;
+	int cnt = 0;
 	while (n--)
 	{
 		cin >> num;
-		uMap.insert({ num, false });
+		uSet.insert(num);
+		cnt++;
 	}
+
 
 	while (m--)
 	{
 		cin >> num;
-		if (uMap.insert({ num, false }).second == false)
-			uMap[num] = true;
+		if (uSet.find(num) != uSet.end())
+		{
+			cnt--;
+			continue;
+		}
+
+		uSet.insert(num);
+		cnt++;
 	}
 
-	int answer = 0;
-	for (auto& m : uMap)
-	{
-		if(m.second == false)
-			answer++;
-	}
-	cout << answer;
+	cout << cnt;
 }
