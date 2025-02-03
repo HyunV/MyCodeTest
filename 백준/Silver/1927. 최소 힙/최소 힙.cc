3d48a804fb35dd0bf5d6294a8f0809b2d96661f4
HyunV <98259740+HyunV@users.noreply.github.com>
2025-02-03 21:45:01 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	int num;
+	while (n--)
+	{
+		cin >> num;
+
+		if (num == 0)
+		{
+			if (pq.empty())
+				cout << 0 << '\n';
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+			pq.push(num);
+	}
+}
