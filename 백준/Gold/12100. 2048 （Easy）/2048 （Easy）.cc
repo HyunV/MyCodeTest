560650b360c403868c7d1542c7bff70af0c45db1
HyunV <98259740+HyunV@users.noreply.github.com>
2025-02-04 21:50:13 +0900

    [Gold I] Title: 2048 (Easy), Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -32,12 +32,8 @@ void rotate(int arr[][MAX_SIZE])
 
 //반시계 회전으로 상,우,하,좌 순으로 배열이 돌아감
 //즉 회전한 배열 기준 숫자를 왼쪽으로 당김
-bool pullPiece(int arr[][MAX_SIZE], int& curBlockSize)
+void pullPiece(int arr[][MAX_SIZE], int& curBlockSize)
 {
-	int copiedArr[MAX_SIZE][MAX_SIZE];
-	copyArr(arr, copiedArr);
-
-	bool isMoved = true;
 	for (int i = 0; i < arrSize; i++)
 	{
 		int widthArr[MAX_SIZE] = {}; //조각이 이동한 상황을 저장하는 배열
@@ -46,14 +42,14 @@ bool pullPiece(int arr[][MAX_SIZE], int& curBlockSize)
 		{
 			if (arr[i][j] == 0)
 				continue;
-			
+
 			//1. 너비 배열에 값이 없다면 최근에 들어온 값을 넣어준다.
 			//2. 너비 배열의 idx내의 value가 새로 옮기는 arr[i][j]의 숫자와 일치하면 *2를 해주고 인덱스를 증가
 			//3. 너비 배열의 idx내의 value가 새로 옮기는 arr[i][j]의 숫자가 다르다면 인덱스를 올리고 삽입시킨다.
-			if (widthArr[idx] == 0) 
+			if (widthArr[idx] == 0)
 				widthArr[idx] = arr[i][j];
 			else if (widthArr[idx] == arr[i][j])
-				widthArr[idx++] *= 2;	
+				widthArr[idx++] *= 2;
 			else if (widthArr[idx] != arr[i][j])
 				widthArr[++idx] = arr[i][j];
 		}
@@ -65,8 +61,6 @@ bool pullPiece(int arr[][MAX_SIZE], int& curBlockSize)
 			curBlockSize = max(curBlockSize, arr[i][j]);
 		}
 	}
-
-	return isSameBoard(arr, copiedArr);
 }
 
 void dfs(int arr[][MAX_SIZE], int depth, int curMaxBlock)
@@ -74,24 +68,24 @@ void dfs(int arr[][MAX_SIZE], int depth, int curMaxBlock)
 	if (depth == MAX_MOVE)
 		return;
 
+	//배열 복사
 	for (int i = 0; i < 4; i++)
 	{
-		//배열 복사
+		rotate(arr); //왼쪽으로 90도 회전시키기
+
 		int tempArr[MAX_SIZE][MAX_SIZE];
 		copyArr(arr, tempArr);
 
-		for (int j = 0; j < i; j++)
-			rotate(tempArr); //왼쪽으로 90도 회전시키기
-
 		int curBlockSize = 0;
-		if (pullPiece(tempArr, curBlockSize))//왼쪽으로 당기기
-			continue;
-		
+		pullPiece(tempArr, curBlockSize);//왼쪽으로 당기기
+
 		maxBlock = max(curBlockSize, maxBlock);
 
+		if (isSameBoard(arr, tempArr))
+			continue;
+
 		//예상 최대값이 작으면 리턴
-		int expectValue = curBlockSize * 1 << (MAX_MOVE - depth + 1);
-		if (expectValue <= maxBlock)
+		if (curBlockSize * pow(2, MAX_MOVE - 1 - depth) <= maxBlock)
 			continue;
 
 		dfs(tempArr, depth + 1, curBlockSize);
@@ -101,6 +95,7 @@ void dfs(int arr[][MAX_SIZE], int depth, int curMaxBlock)
 int main()
 {
 	ios::sync_with_stdio(0);
+	cin.tie(0);
 
 	int arr[MAX_SIZE][MAX_SIZE];
 
@@ -116,5 +111,6 @@ int main()
 	}
 
 	dfs(arr, 0, maxBlock);
+
 	cout << maxBlock;
 }
