a14d311569537a4c4f8c01affa6377ea799ec165
HyunV <98259740+HyunV@users.noreply.github.com>
2025-02-05 00:06:50 +0900

    [Gold V] Title: 회의실 배정, Time: 80 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,33 +1,39 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <algorithm>
-
+#include <math.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<pair<int, int>> vec(n);
+bool compare(const pair<int, int>& p1, const pair<int, int>& p2)
+{
+	if (p1.second == p2.second)
+		return p1.first < p2.first;
 
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i].first >> vec[i].second;
-    }
+	return p1.second < p2.second;
+};
 
-    // 종료 시간을 기준으로 오름차순 정렬 (종료 시간이 같으면 시작 시간 기준 정렬)
-    sort(vec.begin(), vec.end(), [](pair<int, int> a, pair<int, int> b) {
-        if (a.second == b.second) return a.first < b.first;
-        return a.second < b.second;
-    });
+int main()
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>> vec(n);
 
-    int answer = 0;
-    int lastEndTime = 0; // 현재 회의실에서 가장 최근에 끝난 회의의 종료 시간
+	for (int i = 0; i < n; i++)
+		cin >> vec[i].first >> vec[i].second;
 
-    for (int i = 0; i < n; i++) {
-        if (lastEndTime <= vec[i].first) { // 이전 회의 종료 시간보다 새로운 회의 시작 시간이 같거나 크면 배정 가능
-            lastEndTime = vec[i].second;
-            answer++;
-        }
-    }
+	sort(vec.begin(), vec.end(), compare);
 
-    cout << answer;
+	int answer = 0;
+	int lastEnd = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (lastEnd <= vec[i].first)
+		{
+			lastEnd = vec[i].second;
+			answer++;
+		}
+	}
+
+	cout << answer;
 }
