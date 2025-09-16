7093e67baa9ff927c2d86d50148e226bb85a7808
HyunV <98259740+HyunV@users.noreply.github.com>
2025-09-17 00:43:56 +0900

    [Bronze IV] Title: 등교, Time: 0 ms, Memory: 2020 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> vec(n);
+	int cnt = 0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int s, t;
+		cin >> s >> t;
+		if (s + t <= m)
+			vec[cnt++] = s;
+	}
+
+	int answer = vec[0];
+	if (cnt != 0)
+	{
+		for (int i = 0; i < cnt; ++i)
+			if (vec[i] > vec[0])
+				answer = vec[i];
+
+		cout << answer;
+	}
+	else
+		cout << -1;
+}
