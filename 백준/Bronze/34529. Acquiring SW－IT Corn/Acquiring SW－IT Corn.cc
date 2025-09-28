b51729df8653ae8967f4a9ff1a3245b6d3af9080
HyunV <98259740+HyunV@users.noreply.github.com>
2025-09-29 00:54:15 +0900

    [Bronze IV] Title: Acquiring SW-IT Corn, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+	int u,v,w;
+	cin >> x >> y >> z >> u >> v >> w;
+	cout << x * (u / 100) + y * (v / 50) + z * (w /20);
+}
