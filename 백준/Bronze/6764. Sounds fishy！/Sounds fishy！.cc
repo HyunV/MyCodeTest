8d7e1a70cc1a1e483413e0320615b7eb5e11d489
HyunV <98259740+HyunV@users.noreply.github.com>
2025-10-09 04:01:24 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[4];
+	for (int i = 0; i < 4; i++)	{
+		cin >> arr[i];
+	}
+
+	if (arr[0] > arr[1] && arr[1] > arr[2] && arr[2] > arr[3]) {
+		cout << "Fish Diving";
+	}
+	else if (arr[0] < arr[1] && arr[1] < arr[2] && arr[2] < arr[3]) {
+		cout << "Fish Rising";
+	}
+	else if (arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[3]) {
+		cout << "Fish At Constant Depth";
+	}
+	else {
+		cout << "No Fish";
+	}
+}
