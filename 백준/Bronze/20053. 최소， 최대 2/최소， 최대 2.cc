14bc32064911d37b991bc3cd5313962f62ee1c56
HyunV <98259740+HyunV@users.noreply.github.com>
2025-11-05 23:25:58 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 288 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
@@ -1,27 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
 	cin.tie(0);
-
+    
 	int t;
 	cin >> t;
 
-	int n, m, minNum, maxNum;
+	int n, m;
 	while (t--)
 	{
 		cin >> n;
-		minNum = 1'000'001;
-		maxNum = -1'000'001;
+		int minNum = 1'000'001;
+		int maxNum = -1'000'001;
 
 		while (n--)
 		{
 			cin >> m;
-			minNum = minNum > m ? m : minNum;
-			maxNum = maxNum < m ? m : maxNum;
+			minNum = min(minNum, m);
+			maxNum = max(maxNum, m);
 		}
 		cout << minNum << ' ' << maxNum << '\n';
 	}
