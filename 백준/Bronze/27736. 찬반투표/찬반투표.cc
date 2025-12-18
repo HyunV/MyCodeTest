d031f2cd7ae6b0de8391c78e253ec4cd971fddbd
HyunV <98259740+HyunV@users.noreply.github.com>
2025-12-18 21:45:05 +0900

    [Bronze III] Title: 찬반투표, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27736. 찬반투표/찬반투표.cc
+++ 백준/Bronze/27736. 찬반투표/찬반투표.cc
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	int n, m;
 	cin >> n;
 
@@ -14,15 +17,15 @@ int main() {
 		cin >> m;
 		arr[m + 1]++;
 	}
-	
-	if (arr[1] > (n / 2)) { 
+
+	if ((arr[1] * 2) >= n) {
 		// 무효 수가 절반 이상일 시 무효
 		cout << "INVALID";
 	} else if (arr[2] > arr[0]) {
 		// 찬성이 반대보다 많으면 투표가 통과
 		cout << "APPROVED";
-	} else if (arr[0] >= arr[2]) {
-		// 반대가 찬성보다 많고나, 반대와 찬성의 수가 동일하면 투표는 통과되지 않음
+	} else {
+		// 반대가 찬성보다 많거나, 반대와 찬성의 수가 동일하면 투표는 통과되지 않음
 		cout << "REJECTED";
 	}
 }
