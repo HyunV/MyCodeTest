95c31d222ed4b48a8267cf9dd3fad045474ac2cd
HyunV <98259740+HyunV@users.noreply.github.com>
2026-01-04 00:10:28 +0900

    [Bronze III] Title: 수도요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int a, b, c, d, p, x, y;
+    cin >> a >> b >> c >> d >> p;
+    x = a * p;
+
+    if (c < p)
+        y = b + ((p - c) * d);
+    else
+        y = b;
+
+    cout<<min(x, y);
+}
